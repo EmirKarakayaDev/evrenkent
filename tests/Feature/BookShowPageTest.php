ec9e6f1129c2423dea8b6eebd47a36a69c52d12a
Emir Karakaya <dev.emirkarakaya@gmail.com>
2026-08-16 19:45:32 +0300

    Revert "İstatistik şeridini mockup'taki gibi kutulu/büyük ikonlu yap"
    This reverts commit dfa798a9d61cd83d1274f4a2d11f6cad15fc260d.

--- tests/Feature/BookShowPageTest.php
+++ tests/Feature/BookShowPageTest.php
@@ -73,8 +73,7 @@ class BookShowPageTest extends TestCase
 
         $this->get(route('kitaplar.show', $book))
             ->assertOk()
-            ->assertSee('310')
-            ->assertSee('sayfa')
+            ->assertSee('310 sayfa')
             ->assertDontSee('belge')
             ->assertDontSee('video');
     }
